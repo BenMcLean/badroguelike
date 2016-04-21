97d16a9c2044053c1c4e42d1adebc6b38939b432
Benjamin McLean <mclean.ben@gmail.com>
2016-04-21 18:30:02 -0500

    Removed broken squidlib stuff

--- desktop/src/net/benmclean/badroguelike/desktop/DesktopLauncher.java
+++ desktop/src/net/benmclean/badroguelike/desktop/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package net.benmclean.badroguelike.desktop;
 
-import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import net.benmclean.badroguelike.BadRoguelikeGame;
@@ -8,11 +7,11 @@ import net.benmclean.badroguelike.BadRoguelikeGame;
 public class DesktopLauncher {
 	public static void main (String[] args) {
     	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-   		config.width = 50 * 22;
-    	config.height = 32 * 22;
-    	config.addIcon("Tentacle-16.png", Files.FileType.Classpath);
-    	config.addIcon("Tentacle-32.png", Files.FileType.Classpath);
-    	config.addIcon("Tentacle-128.png", Files.FileType.Classpath);
+//   		config.width = 50 * 22;
+//    	config.height = 32 * 22;
+//    	config.addIcon("Tentacle-16.png", Files.FileType.Classpath);
+//    	config.addIcon("Tentacle-32.png", Files.FileType.Classpath);
+//    	config.addIcon("Tentacle-128.png", Files.FileType.Classpath);
     	new LwjglApplication(new BadRoguelikeGame(), config);
     }
 }
