d9fd1ee39b8c02ffb58c53465b22baf3c857edb3
Benjamin McLean <mclean.ben@gmail.com>
2016-04-23 22:07:41 -0500

    abstracted tile size

--- core/src/net/benmclean/badroguelike/view/GameScreen.java
+++ core/src/net/benmclean/badroguelike/view/GameScreen.java
@@ -22,6 +22,8 @@ import net.benmclean.badroguelike.model.GameWorld;
 public class GameScreen implements Screen, Disposable {
     public static final int VIRTUAL_WIDTH = 64;
     public static final int VIRTUAL_HEIGHT = 64;
+    public static final int TILE_WIDTH = 8;
+    public static final int TILE_HEIGHT = 8;
     public Assets assets = new Assets();
     private Color worldBackgroundColor = Color.DARK_GRAY;
     private Color screenBackgroundColor = Color.BLACK;
@@ -41,7 +43,7 @@ public class GameScreen implements Screen, Disposable {
     @Override
     public void show() {
         MapLayers layers = map.getLayers();
-        TiledMapTileLayer layer = new TiledMapTileLayer(world.SIZE_X, world.SIZE_Y, 8, 8);
+        TiledMapTileLayer layer = new TiledMapTileLayer(world.SIZE_X, world.SIZE_Y, TILE_HEIGHT, TILE_WIDTH);
         for (int x = 0; x < world.SIZE_X; x++) {
             for (int y = 0; y < world.SIZE_Y; y++) {
                 StaticTiledMapTile tile=null;
@@ -66,13 +68,13 @@ public class GameScreen implements Screen, Disposable {
         Gdx.gl.glClearColor(worldBackgroundColor.r, worldBackgroundColor.g, worldBackgroundColor.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         worldView.apply();
-        worldView.getCamera().position.set(world.getPlayerX() * 8 + 4, world.getPlayerY() * 8 + 4, 0);
+        worldView.getCamera().position.set(world.getPlayerX() * TILE_HEIGHT + 4, world.getPlayerY() * TILE_WIDTH + 4, 0);
         worldView.update(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
         tiledMapRenderer.setView((OrthographicCamera) worldView.getCamera());
         tiledMapRenderer.render();
         batch.setProjectionMatrix(worldView.getCamera().combined);
         batch.begin();
-        batch.draw(assets.player, world.getPlayerX() * 8, world.getPlayerY() * 8);
+        batch.draw(assets.player, world.getPlayerX() * TILE_HEIGHT, world.getPlayerY() * TILE_WIDTH);
         batch.end();
         frameBuffer.end();
 
